8fde73aa80192c2289d84561e96574bf5d23bbef
gabri0932 <gabrielgarces2432@gmail.com>
2024-03-28 17:49:46 +0100

    moodificaciones

--- app/school/comentar.php
+++ app/school/comentar.php
@@ -10,12 +10,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <link rel="icon" href="../../icons/icon.jpg">
-    <link rel="stylesheet" href="responsive/responsive.css">
+    <link rel="stylesheet" href="../../responsive/responsive.css">
     <link rel="stylesheet" href="../appcss/form_escuelas.css">
     <title>Bautista</title>
 </head>
 <body>
-
+    <?php 
+        require_once("../../responsive/no_responsive.php")
+    ?>
+    <div class="container">
     <?php 
         include("../../data/header.php");
         include("../../data/menu_escuelas.php");     
@@ -43,6 +46,7 @@
         <div class="antiguos">
             <h2>Comentarios antiguos</h2>
         </div>
+    </div>
     </div>
 </body>
 </html>
--- responsive/no_responsive.php
+++ responsive/no_responsive.php
@@ -2,7 +2,7 @@
 <div class="no-responsive">
     <div class="titles">
         <i class='bx bxs-bug'></i>
-        <h2 class="title">Lo sentimos, actualmente Profe Feedback no está disposible para pantallas pequeñas.</h2>
+        <h2 class="title">Lo sentimos, actualmente Profe Feedback no está disponible para tu dispositivo.</h2>
         <h5 class="sub-title" style="font-style: italic;">Intenta hacer conexion desde una pantalla mas grande.</h5>
     </div>
     <div class="button">
