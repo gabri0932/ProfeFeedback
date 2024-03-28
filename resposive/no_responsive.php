e18afcd2deeccb5fc6a6ea0f34412354e4353b55
gabri0932 <gabrielgarces2432@gmail.com>
2024-03-28 02:50:05 +0100

    responsive mas escuelas

---
+++ resposive/no_responsive.php
@@ -0,0 +1,10 @@
+<div class="no-responsive">
+    <div class="titles">
+        <i class='bx bxs-bug'></i>
+        <h2 class="title">Lo sentimos, actualmente Profe Feedback no está disposible para pantallas pequeñas.</h2>
+        <h5 class="sub-title" style="font-style: italic;">De momento solo puede acceder al contenido desde una computadora.</h5>
+    </div>
+    <div class="button">
+        <a href="https://www.google.com">Salir</a>
+    </div>
+</div>
