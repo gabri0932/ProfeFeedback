701aa752d5e039d07c96d2cc9f0f119d35f92e68
gabri0932 <gabrielgarces2432@gmail.com>
2024-05-08 06:24:43 +0200

    el problema

--- resource/pdo.php
+++ resource/pdo.php
@@ -1,6 +1,6 @@
 <?php
-    $pdo = new PDO("mysql:host=localhost; port=3306; dbname=profefeedback;", "root", "1234");
+    //$pdo = new PDO("mysql:host=localhost; port=3306; dbname=profefeedback;", "root", "1234");
     //$pdo = new PDO("mysql:host=localhost; port=3306; dbname=profefeedback;", "root", "Darlin_123");
-  //$pdo = new PDO("mysql:host=localhost; dbname=u117551795_profefeedback;", "u117551795_gabriel", "gabr72_23P");
+    $pdo = new PDO("mysql:host=localhost; dbname=u117551795_profefeedback;", "u117551795_gabriel", "gabr72_23P");
 
 ?>
