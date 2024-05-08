edb2e69fe23c483a00ffb902e36cc93eb0f7c202
gabri0932 <gabrielgarces2432@gmail.com>
2024-05-08 06:40:14 +0200

    desplegue de pagina

--- data/menu_escuelas.php
+++ data/menu_escuelas.php
@@ -16,9 +16,9 @@
                 <ul>
                     <li><i class="white">¿Quieres saber mas?</i>
                         <ul>
-                            <li><a href="../p_privacidad.html" target="_blank"><i class='bx bxs-error-alt'></i> <i class="p_1">Politicas de privacidad</i></a></li>
-                            <li><a href="../nosotros.html" target="_blank"><i class='bx bxl-mailchimp'></i> <i class="p_1">Nosotros</i></a></li>
-                            <li><a href="../Normas.html" target="_blank"><i class='bx bx-book-open'></i> <i class="p_1">Reglas</i></a></li>
+                            <li><a href="../p_privacidad.html"><i class='bx bxs-error-alt'></i> <i class="p_1">Politicas de privacidad</i></a></li>
+                            <li><a href="../nosotros.html"><i class='bx bxl-mailchimp'></i> <i class="p_1">Nosotros</i></a></li>
+                            <li><a href="../Normas.html"><i class='bx bx-book-open'></i> <i class="p_1">Reglas</i></a></li>
                         </ul>
                     </li>
                 </ul>
